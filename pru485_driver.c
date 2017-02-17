29c85566a9a163256ea335c724e0cc96105ed71b
root <root@beaglebone.localdomain>
2017-02-17 10:22:24 -0200

    support for mutexes

--- pru485_driver.c
+++ pru485_driver.c
@@ -9,8 +9,11 @@
 #include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
-//#include <linux/err.h>
 #include <asm/uaccess.h>
+
+#include <linux/mutex.h>
+
+#include <linux/platform_device.h>
 
 #define  DEVICE_NAME "pru485"
 #define  CLASS_NAME  "pru485"
@@ -41,9 +44,27 @@ static struct file_operations fops = {
 		.release = dev_release,
 };
 
+#define DRV_NAME "pruss_uio"
+
+static int pruss485_remove(struct platform_device *dev);
+static int pruss485_probe(struct platform_device *pdev);
+
+static struct platform_driver pruss485_driver = {
+		.probe = pruss485_probe,
+		.remove = pruss485_remove,
+		.driver = {
+				.name = DRV_NAME,
+		},
+};
+
+/* mutex protecting read and writing order */
+static DEFINE_MUTEX(pruchar_mutex);
+
 static int __init pru_driver_init(void) {
 
 	printk(KERN_INFO "PRU KVM: initializing module.\n");
+
+	mutex_init(&pruchar_mutex);
 
 	majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
 	if (majorNumber < 0) {
@@ -65,10 +86,24 @@ static int __init pru_driver_init(void) {
 	prucharDevice = device_create(prucharClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
 	if (IS_ERR(prucharDevice)){
 
+		mutex_destroy(&pruchar_mutex);
 		class_destroy(prucharClass);
 		unregister_chrdev(majorNumber, DEVICE_NAME);
 		printk(KERN_ALERT "PRU KVM: Failed to create the device\n");
 		return PTR_ERR(prucharDevice);
+	}
+
+	if (platform_driver_register(&pruss485_driver)) {
+
+		printk(KERN_INFO "PRU KVM: failed to register platform driver.\n");
+
+		mutex_destroy(&pruchar_mutex);
+
+		device_destroy(prucharClass, MKDEV(majorNumber, 0));
+		class_unregister(prucharClass);
+		class_destroy(prucharClass);
+		unregister_chrdev(majorNumber, DEVICE_NAME);
+
 	}
 
 	printk(KERN_INFO "PRU KVM: device class created correctly\n");
@@ -78,21 +113,32 @@ static int __init pru_driver_init(void) {
 
 static void __exit pru_driver_exit(void) {
 
+	mutex_destroy(&pruchar_mutex);
+
 	device_destroy(prucharClass, MKDEV(majorNumber, 0));
 	class_unregister(prucharClass);
 	class_destroy(prucharClass);
 	unregister_chrdev(majorNumber, DEVICE_NAME);
+	platform_driver_unregister(&pruss485_driver);
 	printk(KERN_INFO "PRU KVM: module closed.\n");
 
 }
 
 static int dev_open(struct inode *inodep, struct file *filep){
 
+	if(!mutex_trylock(&pruchar_mutex)){    /* Try to acquire the mutex */
+		/* returns 1 if successful and 0 if there is contention */
+		printk(KERN_ALERT "PRU KVM: Device in use by another process");
+		return -EBUSY;
+	}
+
 	printk(KERN_INFO "PRU KVM: device has been opened.\n");
 	return 0;
 }
 
 static int dev_release(struct inode *inodep, struct file *filep){
+
+	mutex_unlock(&pruchar_mutex);
 
 	printk(KERN_INFO "PRU KVM: device successfully closed.\n");
 	return 0;
@@ -103,12 +149,12 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
 	// copy_to_user has the format ( * to, *from, size) and returns 0 on success
 	error_count = copy_to_user(buffer, message, size_of_message);
 
-	if (error_count==0) {
-		printk(KERN_INFO "EBBChar: Sent %d characters to the user\n", size_of_message);
+	if (!error_count) {
+		printk(KERN_INFO "PRU KVM: Sent %d characters to the user\n", size_of_message);
 		return (size_of_message=0);
 	}
 	else {
-		printk(KERN_INFO "EBBChar: Failed to send %d characters to the user\n", error_count);
+		printk(KERN_INFO "PRU KVM: Failed to send %d characters to the user\n", error_count);
 		return -EFAULT;
 	}
 }
@@ -119,6 +165,22 @@ static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, lof
 	size_of_message = strlen(message);
 	printk(KERN_INFO "EBBChar: Received %zu characters from the user\n", len);
 	return len;
+}
+
+
+static int pruss485_probe(struct platform_device *pdev) {
+
+	printk(KERN_INFO "PRU KVM: probe function called.\n");
+
+	return 0;
+}
+
+static int pruss485_remove(struct platform_device *dev) {
+
+	printk(KERN_INFO "PRU KVM: remove function called.\n");
+
+        return 0;
+
 }
 
 module_init(pru_driver_init);

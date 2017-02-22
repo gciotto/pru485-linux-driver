a0ce09ccb5704df59b60fec8cb3fe08db473555f
Gustavo Ciotto <gustavociotto@gmail.com>
2017-02-22 10:40:00 -0300

    basic test program

--- uio_pruss_test.c
+++ uio_pruss_test.c
@@ -12,27 +12,29 @@
 #define SZ_12K 0x3000
 
 enum ioctl_cmd {
-	PRUSS_CLEAN,
+	PRUSS_CLEAN = 10,
 	PRUSS_MODE,
-	PRUSS_BAUDRATE,
 	PRUSS_SYNC_STEP,
 	PRUSS_SET_COUNTER,
+	PRUSS_GET_HW_ADDRESS,
+	PRUSS_BAUDRATE,
+	PRUSS_TIMEOUT,
 };
 
 int main () {
 
 	int ret, fd, i;
+	char receive[SZ_12K];
 
 	fd = open("/dev/pruss485", O_RDWR);
 
 	ioctl(fd, PRUSS_CLEAN);
 	ioctl(fd, PRUSS_MODE, 'M');
-	ioctl(fd, PRUSS_SET_COUNTER, 0);
-
-	ioctl(fd, PRUSS_BAUDRATE, 14400);
 	ioctl(fd, PRUSS_SYNC_STEP);
-
-	char receive[SZ_12K];
+	ioctl(fd, PRUSS_SET_COUNTER, 0);
+	ioctl(fd, PRUSS_BAUDRATE, 12);
+	ioctl(fd, PRUSS_GET_HW_ADDRESS);
+	ioctl(fd, PRUSS_TIMEOUT, 8);
 
 	printf("Reading from the device...\n");
 	ret = read(fd, receive, SZ_12K);
@@ -41,13 +43,11 @@ int main () {
 		return errno;
 	}
 
-
-	for (i = 0; i < SZ_12K; i++) {
+	for (i = 0; i < 100; i++) {
 		printf("[%d] = 0x%x ", i, receive[i]);
 
 		if (!((i+1)%10))
 			printf("\n");
-
 	}
 
 	printf("End of the program\n");
